1912fa8ad85b1e86120e3ae8e4591feab8f15c8e
Milton <e0267381@u.nus.edu>
2018-09-09 14:05:32 +0800

    Oauth

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const keys = require('./config/keys');
 
-app.get('/', (req, res)=> {
-    res.send({bye : 'bye'});
-});
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken)=>{
+        console.log(accessToken);
+    })
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 5000;
