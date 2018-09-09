93880a07017b5e933fae08c535fea70a795c2d21
Milton <35495603+yupm@users.noreply.github.com>
2018-09-10 00:05:59 +0800

    Lesson 35

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const app = express();
+const mongoose= require('mongoose');
 const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
+const authRoutes = require('./routes/authRoutes');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-    }, 
-    (accessToken, refreshToken, profile, done)=>{
-        console.log('accessToken: ', accessToken);
-        console.log('refreshToken: ', refreshToken);
-        console.log('profile ', profile);
-    })
-);
+mongoose.connect(keys.mongoURI);
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
+const app = express('mongodb://<dbuser>:<dbpassword>@ds149732.mlab.com:49732/emailydevmil');
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
