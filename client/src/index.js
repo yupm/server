876049f441978e960bd6ca3dbad0f1ae0cf201c3
Milton <35495603+yupm@users.noreply.github.com>
2018-09-11 15:41:10 +0800

    Lecture 76

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,18 @@
+import 'materialize-css/dist/css/materialize.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-think';
 
-ReactDOM.render();
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(<Provider store={store}>
+                    <App />
+                </Provider>, 
+                document.getElementById('root')
+                );
