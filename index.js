de2714ec2ffcde593f30f8a6c75e57d77b807ce6
Milton <35495603+yupm@users.noreply.github.com>
2018-09-12 16:04:28 +0800

    Add Heroku post install build steps for client

--- index.js
+++ index.js
@@ -29,5 +29,20 @@ app.use(passport.session());
 authRoutes(app);
 billRoutes(app);
 
+if(process.env.NODE_ENV ==='production'){
+    // Express will serve up production assets like our main.js file or main.css file
+    //Order of operations, use this first
+    app.use(express.static('client/build'));
+
+
+    //Express will serve up the index.html file if it doesn't recognize the route
+    const path = require('path');
+    //This is a catch all, if no routes, then serve the index.html
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
